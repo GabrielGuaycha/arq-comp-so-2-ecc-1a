066ced61aaee5134735cce20244bb625d37e8247
GabrielGuaycha <gaguaychaes@uide.edu.ec>
2026-04-01 21:10:31 -0500

    01_abr_2026

---
+++ s5.sh
@@ -0,0 +1,12 @@
+# lista archivos y directorios de la carpeta actual en la que estoy
+ls
+# muestra el contenido de un archivo en la terminal
+cat README.md
+#crea un directorio bajo el directorio en el que estoy 
+mkdir padre.txt
+#cambiamos el directorio padre.txt
+cd padre.txt
+#creamos un archivo
+touch hijo.txt
+#me muevo al direcctorio padre del directorio actual
+cd .. 
